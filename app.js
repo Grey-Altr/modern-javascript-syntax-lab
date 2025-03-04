75c5112cea7ef84d555b3dd329419dd78c1de775
GreyA <grey.altaer@gmail.com>
2025-03-04 08:15:46 -0800

    exercise 6 complete

--- app.js
+++ app.js
@@ -105,6 +105,28 @@ myCar.model = 'CX5';
 console.log(myCar);
 
 // ============================================
+
+// ! Exercise 6:
+// a. Define a variable named `propertyName` and assign a string (like 
+//    'username', 'age', or 'email') to it.
+// 
+// b. Create an object named `userProfile`. 
+// 
+// c. Use `propertyName` as a dynamic key in `userProfile`. Assign it a 
+//    relevant value.
+//
+// d. Console log the `userProfile` object to see the result.
+//
+// Your code here:
+
+const propertyName = 'username';
+
+const userProfile = {
+    [propertyName]: 'GAltaer',
+};
+
+console.log(userProfile);
+
 // ============================================
 // ============================================
 // ============================================
